d4825348a266730a4297ab39df64c7b738368979
ADarien <aalian.darien@gmail.com>
2020-12-18 16:15:44 +0700

    add martian daytime

---
+++ golangs.org/interface/sample_applying/curiosity.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type stardater interface {
+	YearDay() int
+	Hour() int
+}
+
+func stardate(t stardater) float64 {
+	day := float64(t.YearDay())
+	h := float64(t.Hour()) / 24.0
+	return 1000 + day + h
+}
+
+type sol int
+
+// martian year
+func (s sol) YearDay() int {
+	return int(s % 668)
+}
+
+// unknown hour
+func (s sol) Hour() int {
+	return 0
+}
+
+func main() {
+	day := time.Date(2012, 8, 6, 5, 17, 0, 0, time.UTC)
+	fmt.Printf("%.1f Curiosity has landed\n", stardate(day))
+
+	s := sol(1422)
+	fmt.Printf("%.1f Happy birthday\n", stardate(s))
+}
