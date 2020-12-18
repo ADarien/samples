de9dbe7e5716f61f54070fa24b2a5efeeb5b44ab
ADarien <aalian.darien@gmail.com>
2020-12-18 14:20:13 +0700

    add starship & rover

--- golangs.org/interface/interface.go
+++ golangs.org/interface/interface.go
@@ -5,20 +5,46 @@ import (
 	"strings"
 )
 
+// talk
 var t interface {
 	talk() string
 }
 
 type martian struct{}
 
+// martian talk
 func (m martian) talk() string {
 	return "nack nack"
 }
 
 type laser int
 
+// laser talk
 func (l laser) talk() string {
 	return strings.Repeat("pew ", int(l))
+}
+
+// тот, кто говорит
+type talker interface {
+	talk() string
+}
+
+// выстрел с параметром talker
+func shout(t talker) {
+	louder := strings.ToUpper(t.talk())
+	fmt.Println(louder)
+}
+
+type rover string
+
+// rover talk
+func (r rover) talk() string {
+	return string(r)
+}
+
+// встраиваем laser в starship
+type starship struct {
+	laser
 }
 
 func main() {
@@ -27,4 +53,15 @@ func main() {
 
 	t = laser(3)
 	fmt.Println(t.talk())
+
+	shout(martian{})
+	shout(laser(2))
+
+	r := rover("whir whir")
+	shout(r)
+
+	//когда говорит starship, за него говорит laser
+	s := starship{laser(3)}
+	fmt.Println(s.talk())
+	shout(s)
 }
